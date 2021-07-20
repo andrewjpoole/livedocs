ed282e34b281767ce4cdbc4f045023325a31c03f
Phil Doyle <phil.doyle@clear.bank>
2021-07-20 15:17:27 +0100

    Add persistent scroll position

---
+++ LiveDocs/wwwroot/scroll-interface.js
@@ -0,0 +1,10 @@
+﻿export function getScroll() {
+    return {
+        X: window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft || 0,
+        Y: window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
+    };
+}
+
+export function setScroll(left, top) {
+    window.scrollTo(left, top);
+}
