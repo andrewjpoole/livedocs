3155078afda9520fa1f4c42e65bd6efd7aed9ae7
Andrew Poole <andrew.poole@clear.bank>
2021-07-22 04:30:01 +0100

    added github-markdown like css, switched manual links to css and js to UI.ResourceDeclare, temporariy disabled resource reloading until scheduler is fixed.

--- LiveDocs.Server/Startup.cs
+++ LiveDocs.Server/Startup.cs
@@ -27,8 +27,6 @@ namespace LiveDocs.Server
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<StronglyTypedConfig.LiveDocs>(Configuration.GetSection(StronglyTypedConfig.LiveDocs.ConfigKey));
@@ -50,8 +48,7 @@ namespace LiveDocs.Server
             services.AddSingleton<IAggregatorBackgroundService, AggregatorBackgroundService>();
             //services.AddHostedService(sp => (AggregatorBackgroundService)sp.GetService<IAggregatorBackgroundService>());
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
